f29d4e5c0d2a3d4061d0613f365d4b7fc01e7816
Gajanan Bidwai <gajananbidwai525@gmail.com>
2026-01-02 15:45:53 +0530

    Initial commit

---
+++ MediaInSwiftUI/MediaInSwiftUI/MediaInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaInSwiftUIApp.swift
+//  MediaInSwiftUI
+//
+//  Created by Neosoft on 02/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MediaInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
