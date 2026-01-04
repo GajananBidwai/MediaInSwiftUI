d1c7e8ac38d815d59b358faff93e63538123e0f6
Gajanan Bidwai <gajananbidwai525@gmail.com>
2026-01-04 21:04:23 +0530

    Save picture in a photo library

--- MediaInSwiftUI/MediaInSwiftUI/ContentView.swift
+++ MediaInSwiftUI/MediaInSwiftUI/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
 //    @State private var picture: UIImage?
     @State private var path = NavigationPath()
     @State private var picture: UIImage?
+    @State private var showAlert: Bool = false
     
     var body: some View {
 //        NavigationStack {
@@ -40,11 +41,27 @@ struct ContentView: View {
         NavigationStack(path: $path) {
             VStack {
                 HStack {
+                    Button("Share Picture") {
+                        showAlert = true
+                    }.disabled(picture == nil ? true : false)
                     Spacer()
                     NavigationLink("Get Picture", value: "Open picture")
                 }.navigationDestination(for: String.self) { _ in
                     ImagePicker(path: $path, picture: $picture)
                 }
+                .alert("Save Picture", isPresented: $showAlert) {
+                    Button("Cancel", role: .cancel) {
+                        showAlert = false
+                    }
+                    Button("Yes", role: .none) {
+                        if let picture {
+                            UIImageWriteToSavedPhotosAlbum(picture, nil, nil, nil)
+                        }
+                    }
+                } message: {
+                    Text("Do you want to store the picture in photos library?")
+                }
+
                 Image(uiImage: (picture ?? UIImage(named: "noImage"))!)
                     .resizable()
                     .scaledToFill()
